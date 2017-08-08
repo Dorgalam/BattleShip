926acda06914070dd9ec24e5fbc04c4f3c4c843d
Dorgalam <dorgalam@gmail.com>
2017-08-08 22:30:11 +0300

    Some new files

--- src/com/battleship/Main.java
+++ src/com/battleship/Main.java
@@ -3,6 +3,7 @@ package com.battleship;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Utils a = new Utils();
+        System.out.println(a.checkXML("src/resources/battleShip_5_basic.xml"));
     }
 }
--- src/com/battleship/Utils.java
+++ src/com/battleship/Utils.java
@@ -10,13 +10,41 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+
 import org.xml.sax.SAXException;
 
 class Utils {
     boolean checkXML(String xmlPath) {
+        return schemaIsValid(xmlPath) && dataIsValid(xmlPath);
+    }
+
+    private boolean dataIsValid(String xmlPath) {
+        try {
+            File fXmlFile = new File(xmlPath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            NodeList boards = doc.getElementsByTagName("board");
+            int boardSize = Integer.parseInt(doc.getElementsByTagName("boardSize").item(0).getTextContent());
+            isBoardValid(boards.item(0), boardSize);
+            isBoardValid(boards.item(1), boardSize);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+    private boolean isBoardValid(Node board, int boardSize) {
+
+        System.out.println(boardSize);
         return false;
     }
-    boolean checkSchema(String xmlPath){
+    private boolean schemaIsValid(String xmlPath){
         try {
             SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
             Schema schema = factory.newSchema(new File("src/resources/BattleShipGame.xsd"));
